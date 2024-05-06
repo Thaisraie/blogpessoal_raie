72bde7d08f8f21a2f3e7341bf222ff6d62c4314d
raiedev <thaisdivino21@gmail.com>
2024-05-06 12:11:08 -0300

    Atualização do deploy - Blog Raie

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,13 @@ import { PostagemModule } from './postagem/postagem.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: ProdService,
+      useClass: DevService,
       imports: [ConfigModule],
     }),
     PostagemModule,
